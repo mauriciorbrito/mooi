4378822d93390a461b85410f28fd076f0703cab5
mauricio.brito <maurodbri@gmail.com>
2017-05-10 12:39:22 -0500

    Primera página controler/home y view/index en PHP

---
+++ application/controllers/home.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class home extends CI_Controller {
+
+
+
+}
---
+++ application/views/index.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>mooi</title>
+
+</head>
+<body>
+
+<div id="container">
+    mooi
+</div>
+
+</body>
+</html>
